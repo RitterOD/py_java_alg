b4ae0e9c7bf254e71d1c267841727220108d7628
AlexanderMaslov <ritterod@gmail.com>
2020-10-03 18:21:40 +0300

    py_alg: add get way function and fix code-style in toroitse_dynp
    Signed-off-by: Maslov Alexander <ritterod@gmail.com>

--- alex/tortoise_dynp.py
+++ alex/tortoise_dynp.py
@@ -1,30 +1,33 @@
 from random import *
 
 class TortoiseTask:
-    def __init__(self, n, m, min_price, max_price):
-        self.n = n
-        self.m = m
+    def __init__(self, row_amnt, col_amnt, min_price, max_price):
+        self.row_amnt = row_amnt
+        self.col_amnt = col_amnt
         self.min_price = min_price
         self.max_price = max_price
         self.field = None
-        self.solve_filed = None
+        self.solve_field = None
 
     def generate_field(self):
         field = []
-        for row_ind in range(self.n):
-            field.append([0] * self.m)
+        for row_ind in range(self.row_amnt):
+            field.append([0] * self.col_amnt)
         for row_ind in range(len(field)):
             for col_ind in range(len(field[row_ind])):
                 field[row_ind][col_ind] = randint(self.min_price, self.max_price)
         self.field = field
 
-    def getField(self):
+    def get_field(self):
         return self.field
+
+    def get_solve_field(self):
+        return self.solve_field
 
     def solve(self):
         solve_field = []
-        for row_ind in range(self.n):
-            solve_field.append([0] * self.m)
+        for row_ind in range(self.row_amnt):
+            solve_field.append([0] * self.col_amnt)
         solve_field[0][0] = self.field[0][0]
         for col_num in range(1, len(self.field[0])):
             solve_field[0][col_num] = solve_field[0][col_num - 1] + self.field[0][col_num]
@@ -38,19 +41,42 @@ class TortoiseTask:
                     solve_field[row_num][col_num] = first_val
                 else:
                     solve_field[row_num][col_num] = second_val
-        print("solve field\n")
-        for row in solve_field:
-            print(row)
-        print("end solve field\n")
-        self.solve_filed = solve_field
-        return solve_field[self.n - 1][self.m - 1]
+        self.solve_field = solve_field
+        return solve_field[self.row_amnt - 1][self.col_amnt - 1]
+
+    def getWay(self):
+        cur_row = self.row_amnt - 1
+        cur_col = self.col_amnt - 1
+        cur_sum = self.solve_field[cur_row][cur_col]
+        way = [(cur_row, cur_col)]
+        while cur_row != 0 or cur_col != 0:
+            next_sum = cur_sum - self.field[cur_row][cur_col]
+            if (next_sum == self.solve_field[cur_row - 1][cur_col] and
+                    cur_row - 1 >= 0):
+                cur_row -= 1
+            elif (next_sum == self.solve_field[cur_row][cur_col - 1] and
+                    cur_col - 1 >= 0):
+                cur_col -= 1
+            cur_sum = next_sum
+            way.append((cur_row, cur_col))
+        way.reverse()
+        return way
+
 
 if __name__ == "__main__":
-    tortoiseTask = TortoiseTask(3, 3, 1, 9)
+    tortoiseTask = TortoiseTask(5, 10, 1, 9)
     tortoiseTask.generate_field()
-    field = tortoiseTask.getField()
+    field = tortoiseTask.get_field()
+    print("field\n")
     for row in field:
         print(row)
+    print("end field")
     rv = tortoiseTask.solve()
-
-    print("solution =", rv)
+    print("\nsolution =", rv, "\n")
+    rv = tortoiseTask.getWay()
+    solve_field = tortoiseTask.get_solve_field()
+    print("solve field")
+    for row in solve_field:
+        print(row)
+    print("end solve field\n")
+    print("Way =", rv)
