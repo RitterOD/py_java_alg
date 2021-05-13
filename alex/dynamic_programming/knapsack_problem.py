936c7f93aab709aad35a5563287cdea4ea25a460
Maslow Victor <maslowik@yandex.ru>
2021-05-13 20:37:18 +0300

    py_alg: add knapsack proble partial solution
    Signed-off-by: Maslov Alexander <ritterod@gmail.com

---
+++ alex/dynamic_programming/knapsack_problem.py
@@ -0,0 +1,33 @@
+
+def solve(knapsack_weigt, weights, prices):
+    v = [[] for ind in range(len(weights))]
+    print(v)
+    print("0 step")
+    prev_step_val = [x // weights[0] * prices[0] for x in range(0, knapsack_weigt + 1)]
+    print(prev_step_val)
+    print(len(prev_step_val))
+    cur_step_val = []
+    for step in range(1, len(weights)):
+        cur_step_val = []
+        for x in range(0, knapsack_weigt + 1):
+            if x < weights[step]:
+                cur_step_val.append(prev_step_val[x])
+            else:
+                chose_cur = cur_step_val[x - weights[step]] + prices[step]
+                if chose_cur > prev_step_val[x]:
+                    cur_step_val.append(chose_cur)
+                else:
+                    cur_step_val.append(prev_step_val[x])
+        prev_step_val = cur_step_val
+        print(str(step) + " step")
+        print(prev_step_val)
+    return cur_step_val[knapsack_weigt]
+
+
+if __name__ == '__main__':
+    print('knapsack problem')
+    knapsack_weigt = 17
+    weights = [3, 4, 5]
+    prices = [4, 6, 7]
+    solution = solve(knapsack_weigt, weights, prices)
+    print(solution)
