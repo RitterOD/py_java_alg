f9b521a7645ef5bd87ee68ac004a190bb400eb96
Maslow Victor <maslowik@yandex.ru>
2021-05-15 19:50:05 +0300

    py_alg: fix merge sort implementation
    Signed-off-by: Maslov Alexander <ritterod@gmail.com>

--- alex/sort/merge_sort.py
+++ alex/sort/merge_sort.py
@@ -2,17 +2,30 @@ from array import array
 
 
 def merge(arr: list, left: int, mid:int, right: int) -> array:
-    cur_sort = left
+    tmp_arr = [0] * (right - left)
+    print('left =', arr[left:mid])
+    print('right =', arr[mid:right])
+    cur_ind = 0
     cur_left = left
     cur_right = mid
-    while cur_sort != right:
-        if arr[cur_left] <= arr[cur_right]:
-            cur_sort +=1
-            cur_left +=1
+    while cur_ind != (right - left):
+        if cur_left != mid and cur_right != right:
+            if arr[cur_left] < arr[cur_right]:
+                tmp_arr[cur_ind] = arr[cur_left]
+                cur_left += 1
+            else:
+                tmp_arr[cur_ind] = arr[cur_right]
+                cur_right += 1
+        elif cur_left == mid:
+            tmp_arr[cur_ind] = arr[cur_right]
+            cur_right += 1
         else:
-            tmp = arr[cur_left]
-            arr[cur_left] = arr[cur_right]
-            arr[cur_right] = tmp
+            tmp_arr[cur_ind] = arr[cur_left]
+            cur_left += 1
+        cur_ind += 1
+    print(tmp_arr)
+    for ind in range(left, right):
+        arr[ind] = tmp_arr[ind - left]
     return arr
 
 
@@ -22,13 +35,14 @@ def merge_sort(arr: list, left: int, right: int) -> None:
     else:
         mid = (left + right) // 2
         merge_sort(arr, left, mid)
-        merge_sort(arr, mid + 1, right)
+        merge_sort(arr, mid, right)
         merge(arr, left, mid, right)
 
 
 if __name__ == '__main__':
     print('Merge sort')
     tst = [4, 5, 3, 0, 1, 2]
+    #tst = [4, 5, 3]
     print('before =', tst)
     merge_sort(tst, 0, len(tst))
     print('after =', tst)
